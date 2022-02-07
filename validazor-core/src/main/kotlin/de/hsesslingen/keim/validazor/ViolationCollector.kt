35d83180d252146d91aa632b02808d080daf3053
Ben Oesch <ben.oesch@hs-esslingen.de>
2022-02-07 17:24:11 +0100

    Added convenience method to ViolationCollector which generations constraint info from annotation.

--- validazor-core/src/main/kotlin/de/hsesslingen/keim/validazor/ViolationCollector.kt
+++ validazor-core/src/main/kotlin/de/hsesslingen/keim/validazor/ViolationCollector.kt
@@ -8,6 +8,13 @@ interface ViolationCollector {
      * Adds a violation to this [ViolationCollector].
      */
     fun add(message: String, path: PropertyPath, constraintInfo: ConstraintInfo)
+
+    /**
+     * Adds a violation to this [ViolationCollector] with [ConstraintInfo] generated from [constraint].
+     */
+    fun add(message: String, path: PropertyPath, constraint: Annotation) {
+        add(message, path, constraint.toConstraintInfo())
+    }
 }
 
 /**
