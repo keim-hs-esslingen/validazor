6939c38e49b2c645c1a3e83829df12c4340b69e2
Ben Oesch <ben.oesch@hs-esslingen.de>
2022-02-14 11:06:30 +0100

    Added copies of the temporal constraints.

---
+++ validazor-common-constraints/src/main/kotlin/de/hsesslingen/keim/validazor/constraints/Future.kt
@@ -0,0 +1,79 @@
+package de.hsesslingen.keim.validazor.constraints
+
+import de.hsesslingen.keim.validazor.*
+import java.time.*
+import java.time.chrono.HijrahDate
+import java.time.chrono.JapaneseDate
+import java.time.chrono.MinguoDate
+import java.time.chrono.ThaiBuddhistDate
+import java.util.*
+
+/**
+ * Validates whether the annotated field value is in the future.
+ *
+ * For comparison, the [NowContext] of the validation is used, which can be specialized when calling
+ * [Validazor.validate].
+ *
+ * Supported types:
+ * - [Instant]
+ * - [LocalTime]
+ * - [LocalDate]
+ * - [LocalDateTime]
+ * - [OffsetTime]
+ * - [OffsetDateTime]
+ * - [ZonedDateTime]
+ * - [Year]
+ * - [YearMonth]
+ * - [Month]
+ * - [MonthDay]
+ * - [HijrahDate]
+ * - [MinguoDate]
+ * - [JapaneseDate]
+ * - [ThaiBuddhistDate]
+ * - [Long] (interpreted as epoch millis)
+ * - [Date]
+ * - [Calendar]
+ *
+ * `null` is considered valid.
+ */
+annotation class Future {
+
+    /**
+     * [ConstraintValidator] for the [Future] constraint.
+     */
+    class Validator : ConstraintValidator<Future> {
+        override fun validate(
+            constraint: Future,
+            value: Any?,
+            path: PropertyPath,
+            violations: ViolationCollector,
+            returnOnFirstViolation: Boolean,
+            now: NowContext
+        ) {
+            checkConstraint("must be in future", path, constraint, violations) {
+                when (value) {
+                    is Instant -> value.isAfter(now.asInstant)
+                    is LocalTime -> value.isAfter(now.asLocalTime)
+                    is LocalDateTime -> value.isAfter(now.asLocalDateTime)
+                    is LocalDate -> value.isAfter(now.asLocalDate)
+                    is OffsetDateTime -> value.isAfter(now.asOffsetDateTime)
+                    is OffsetTime -> value.isAfter(now.asOffsetTime)
+                    is ZonedDateTime -> value.isAfter(now.asZonedDateTime)
+                    is Year -> value.isAfter(now.asYear)
+                    is YearMonth -> value.isAfter(now.asYearMonth)
+                    is Month -> value.value > now.asMonth.value
+                    is MonthDay -> value.isAfter(now.asMonthDay)
+                    is HijrahDate -> value.isAfter(now.asHijrahDate)
+                    is MinguoDate -> value.isAfter(now.asMinguoDate)
+                    is JapaneseDate -> value.isAfter(now.asJapaneseDate)
+                    is ThaiBuddhistDate -> value.isAfter(now.asThaiBuddhistDate)
+                    is Long -> value > now.asEpochMilli
+                    // Use converted values for Date and Calendar to account for differences in implementation to java.time.*
+                    is Date -> value.time > now.asDate.time
+                    is Calendar -> value.timeInMillis > now.asCalendar.timeInMillis
+                    else -> true
+                }
+            }
+        }
+    }
+}
---
+++ validazor-common-constraints/src/main/kotlin/de/hsesslingen/keim/validazor/constraints/Past.kt
@@ -0,0 +1,78 @@
+package de.hsesslingen.keim.validazor.constraints
+
+import de.hsesslingen.keim.validazor.*
+import java.time.*
+import java.time.chrono.HijrahDate
+import java.time.chrono.JapaneseDate
+import java.time.chrono.MinguoDate
+import java.time.chrono.ThaiBuddhistDate
+import java.util.*
+/**
+ * Validates whether the annotated field value is in the past.
+ *
+ * For comparison, the [NowContext] of the validation is used, which can be specialized when calling
+ * [Validazor.validate].
+ *
+ * Supported types:
+ * - [Instant]
+ * - [LocalTime]
+ * - [LocalDate]
+ * - [LocalDateTime]
+ * - [OffsetTime]
+ * - [OffsetDateTime]
+ * - [ZonedDateTime]
+ * - [Year]
+ * - [YearMonth]
+ * - [Month]
+ * - [MonthDay]
+ * - [HijrahDate]
+ * - [MinguoDate]
+ * - [JapaneseDate]
+ * - [ThaiBuddhistDate]
+ * - [Long] (interpreted as epoch millis)
+ * - [Date]
+ * - [Calendar]
+ *
+ * `null` is considered valid.
+ */
+annotation class Past {
+
+    /**
+     * [ConstraintValidator] for the [Past] constraint.
+     */
+    class Validator : ConstraintValidator<Past> {
+        override fun validate(
+            constraint: Past,
+            value: Any?,
+            path: PropertyPath,
+            violations: ViolationCollector,
+            returnOnFirstViolation: Boolean,
+            now: NowContext
+        ) {
+            checkConstraint("must be in past", path, constraint, violations) {
+                when (value) {
+                    is Instant -> value.isBefore(now.asInstant)
+                    is LocalTime -> value.isBefore(now.asLocalTime)
+                    is LocalDateTime -> value.isBefore(now.asLocalDateTime)
+                    is LocalDate -> value.isBefore(now.asLocalDate)
+                    is OffsetDateTime -> value.isBefore(now.asOffsetDateTime)
+                    is OffsetTime -> value.isBefore(now.asOffsetTime)
+                    is ZonedDateTime -> value.isBefore(now.asZonedDateTime)
+                    is Year -> value.isBefore(now.asYear)
+                    is YearMonth -> value.isBefore(now.asYearMonth)
+                    is Month -> value.value < now.asMonth.value
+                    is MonthDay -> value.isBefore(now.asMonthDay)
+                    is HijrahDate -> value.isBefore(now.asHijrahDate)
+                    is MinguoDate -> value.isBefore(now.asMinguoDate)
+                    is JapaneseDate -> value.isBefore(now.asJapaneseDate)
+                    is ThaiBuddhistDate -> value.isBefore(now.asThaiBuddhistDate)
+                    is Long -> value < now.asEpochMilli
+                    // Use converted values for Date and Calendar to account for differences in implementation to java.time.*
+                    is Date -> value.time < now.asDate.time
+                    is Calendar -> value.timeInMillis < now.asCalendar.timeInMillis
+                    else -> true
+                }
+            }
+        }
+    }
+}
